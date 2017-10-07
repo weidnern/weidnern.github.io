d06593f8724657f9349477d6de16769cb9680cd8
Nick Weidner <weidnerjn@gmail.com>
2017-10-07 13:18:57 -0500

    Moving cover photo

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+var movementStrength = 25;
+var height = movementStrength / $(window).height();
+var width = movementStrength / $(window).width();
+$(".wide").mousemove(function(e){
+          var pageX = e.pageX - ($(window).width() / 2);
+          var pageY = e.pageY - ($(window).height() / 2);
+          var newvalueX = width * pageX * -1 - 25;
+          var newvalueY = 1050 + (height * pageY * -1 - 50);
+          //var newvalueX = 0;
+          //var newvalueY = 1200;
+          $('.wide').css("background-position", newvalueX+"px     "+newvalueY+"px");
+});
+});
+
+$(document).ready(function() {
+var movementStrength = 45;
+var height = movementStrength / $(window).height();
+var width = movementStrength / $(window).width();
+$(".wide2").mousemove(function(e){
+          var pageX = e.pageX - ($(window).width() / 2);
+          var pageY = e.pageY - ($(window).height() / 2);
+          var newvalueX = width * pageX * -1 - 25;
+          var newvalueY = 1050 + (height * pageY * -1 - 50);
+          //var newvalueX = 0;
+          //var newvalueY = 1200;
+          $('.wide2').css("background-position", newvalueX+"px     "+newvalueY+"px");
+});
+});
