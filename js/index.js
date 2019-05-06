d690091dc1c9d8b2eb5f817c5d0602484d967159
Weidner <weid691@tamu.edu>
2019-05-06 17:15:00 -0500

    Forgot to set correct JS script for cover photo movement

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
-function moveMe() {
-	$('#me').css({
-		//'background-position': '100px 60%' 
-		'transform': 'translate( 30px , 30px)'
-	});
-}
-
-$('#me').mousemove(function(event) {
-	//var x = event.pageX - $('#me').offset().left;
-	//var y = evt.pageY - $('#me').offset().top;	
-	//console.log(x);
-	
-	moveMe();
+$("#container-nz").mousemove(function(e) {
+  parallaxIt(e, ".nzme", -100);
+  parallaxIt(e, ".nzname", -50);
+  parallaxIt(e, ".nzbg", -30);
+  
 });
+
+function parallaxIt(e, target, movement) {
+  var $this = $("#container-nz");
+  var relX = e.pageX - $this.offset().left;
+  var relY = e.pageY - $this.offset().top;
+
+  TweenMax.to(target, 1, {
+    x: (relX - $this.width() / 2) / $this.width() * movement,
+    y: (relY - $this.height() / 2) / $this.height() * movement
+  });
+}
